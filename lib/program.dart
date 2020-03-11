3c8ab4b6629c6cc3e3ca126f7f207c715dee657c
unknown <nicky.lim.yf@fusionex.local>
2020-03-11 22:42:05 +0800

    init

---
+++ lib/program.dart
@@ -0,0 +1,3 @@
+import 'package:flutter_template/startup.dart';
+
+void main() => startup();
---
+++ lib/startup.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template/features/main.dart';
+
+startup() => runApp(MyApp());
