02d7c71a4b0ce18ded18007531759f15ac52d080
Calvin Low <calvinlow@MacBook-Pro-3.local>
2020-03-16 22:22:37 +0800

    Change env setting loading to program.dart

--- lib/startup.dart
+++ lib/startup.dart
@@ -1,39 +1,12 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_template/domain/app_settings.dart';
 import 'package:flutter_template/features/app.dart';
 import 'package:flutter_template/infrastructure/locator.dart';
-import 'package:flutter_template/seedwork/map_util.dart';
 
 const String APP_SETTING_JSON = 'appsettings.json';
 const String APP_SETTING_LOCAL_JSON = 'appsettings.local.json';
 
-startup() {
-  WidgetsFlutterBinding.ensureInitialized();
-  loadEnvSettings()
-      .then((Map<String, dynamic> envConfig) => AppSettings.fromJson(envConfig))
-      .then((AppSettings appSettings) => setupLocator(appSettings))
-      .then((_) => runApp(App()));
-}
-
-Future<Map<String, dynamic>> loadEnvSettings() async {
-  List<Map<String, dynamic>> appSettings = await Future.wait([
-    loadJson(APP_SETTING_JSON),
-    loadJson(APP_SETTING_LOCAL_JSON),
-  ]);
-
-  return MapUtil.mergeMap(appSettings);
-}
-
-Future<Map<String, dynamic>> loadJson(String fileName) async {
-  try {
-    return rootBundle.loadStructuredData<Map<String, dynamic>>(
-      fileName,
-      (String settings) async => jsonDecode(settings),
-    );
-  } catch (_) {
-    return {};
-  }
+void startup(AppSettings appSettings) {
+  setupLocator(appSettings);
+  runApp(App());
 }
