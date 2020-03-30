55aae9482e8f25a0a3526fdc080401005a7e7588
Calvin Low Khai Wynn <calvin.low.kw@fusionex.local>
2020-03-30 12:55:48 +0800

    Update http_record_repository

--- lib/infrastructure/repository/http_record_repository.dart
+++ lib/infrastructure/repository/http_record_repository.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_template/model/record.dart';
-import 'package:flutter_template/infrastructure/repository/base_repository.dart';
+import 'package:flutter_template/model/record_repository.dart';
+import 'package:flutter_template/seedwork/http_repository.dart';
 
-class RecordRepository extends BaseRepository {
-  RecordRepository(String baseUrl) : super(baseUrl);
+class HttpRecordRepository extends HttpRepository implements RecordRepository {
+  HttpRecordRepository(String baseUrl) : super(baseUrl);
 
+  @override
   Future<List<Record>> list(int pageSize, int page) async {
     List<dynamic> records = await super.getAll(
       '/record',
@@ -14,24 +16,28 @@ class RecordRepository extends BaseRepository {
     return records.map((dynamic record) => Record.fromJson(record)).toList();
   }
 
+  @override
   Future<Record> read(String id) async {
     dynamic record = await super.get("/record/$id");
 
     return Record.fromJson(record);
   }
 
+  @override
   Future<Record> add(Record record) async {
     dynamic newRecord = await super.post('/record', record.toJson());
 
     return Record.fromJson(newRecord);
   }
 
+  @override
   Future<Record> update(String id, Record record) async {
     dynamic updatedRecord = await super.put("/record/$id", record.toJson());
 
     return Record.fromJson(updatedRecord);
   }
 
+  @override
   Future<void> remove(String id) {
     return super.delete("/record/$id");
   }
