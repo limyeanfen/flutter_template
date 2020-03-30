39babca34d289dbfbf7eb1ab4226bb35e94b8af5
Calvin Low Khai Wynn <calvin.low.kw@fusionex.local>
2020-03-30 14:55:06 +0800

    Added i18n support

--- lib/features/main/home_screen.dart
+++ lib/features/main/home_screen.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/domain/i18n.dart';
 import 'package:flutter_template/infrastructure/router.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    I18n i18n = I18n.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Demo'),
+        title: Text(i18n.mainScreenTitle),
       ),
       body: Center(
         child: RaisedButton(
-          child: Text('Check Record Summary'),
+          child: Text(i18n.mainScreenCheckRecordSummary),
           onPressed: () =>
               Navigator.of(context).pushNamed(Router.recordSummaryScreenRoute),
         ),
--- lib/features/record/record_summary_screen.dart
+++ lib/features/record/record_summary_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_template/domain/i18n.dart';
 import 'package:flutter_template/features/record/component/record_summary_list.dart';
 import 'package:flutter_template/infrastructure/locator.dart';
 import 'package:flutter_template/infrastructure/viewmodel/record/record_summary_screen_view_model.dart';
@@ -8,11 +9,12 @@ class RecordSummaryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     RecordSummaryScreenViewModel recordSummaryScreenViewModel = locator.get();
+    I18n i18n = I18n.of(context);
     return ChangeNotifierProvider<RecordSummaryScreenViewModel>(
       create: (_) => recordSummaryScreenViewModel,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Record Summary'),
+          title: Text(i18n.recordSummaryScreenTitle),
         ),
         body: RecordSummaryList(),
       ),
