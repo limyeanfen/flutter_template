38b74afb9fad9d5b2c7758e1c9eea1f32f0e0ecd
Calvin Low Khai Wynn <calvin.low.kw@fusionex.local>
2020-03-30 12:57:03 +0800

    Update locator on initializing record repository

--- lib/infrastructure/locator.dart
+++ lib/infrastructure/locator.dart
@@ -1,9 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_template/infrastructure/repository/static_record_repository.dart';
 import 'package:flutter_template/model/app_settings.dart';
-import 'package:flutter_template/infrastructure/repository/record_repository.dart';
+import 'package:flutter_template/model/record.dart';
+import 'package:flutter_template/model/record_repository.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.I;
 
-void setupLocator(AppSettings appSettings) {
-  locator.registerSingleton(RecordRepository(appSettings.baseUrl));
+Future<void> setupLocator(AppSettings appSettings) async {
+  List<Record> staticRecords = await _loadRecord();
+
+  locator.registerSingleton<RecordRepository>(
+    StaticRecordRepository(staticRecords),
+  );
+}
+
+Future<List<Record>> _loadRecord() {
+  return rootBundle.loadStructuredData<List<Record>>(
+    'static/data/records.json',
+    (String json) async => (jsonDecode(json) as List<dynamic>)
+        .map((dynamic recordJson) => Record.fromJson(recordJson))
+        .toList(),
+  );
 }
